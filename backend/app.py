fe4c468bdee9e6755cc84af631b77e5df00fcf46
Siddharth <siddharth.shukla@adypu.edu.in>
2025-10-06 14:36:51 +0530

    updating app.py

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import heapq
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -30,4 +31,5 @@ def schedule():
     return jsonify({"results": results, "total_rooms": total_rooms})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
