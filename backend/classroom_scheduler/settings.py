8ea44babbe93ac2ba181dec3fe873646fc514357
Siddharth <siddharth.shukla@adypu.edu.in>
2026-02-10 15:47:21 +0530

    Adding Rest framework

--- backend/classroom_scheduler/settings.py
+++ backend/classroom_scheduler/settings.py
@@ -136,3 +136,8 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
+    'DEFAULT_PERMISSION_CLASSES': [],
+}
