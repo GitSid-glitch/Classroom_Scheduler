6506dec3172e13bd84498ed45addce745c229907
Siddharth <siddharth.shukla@adypu.edu.in>
2026-02-12 12:26:16 +0530

    Updating the deconstructing object

--- classroom-scheduler-frontend/src/components/ScheduleView.jsx
+++ classroom-scheduler-frontend/src/components/ScheduleView.jsx
@@ -2,15 +2,16 @@ export default function ScheduleView({ scheduleResult }) {
     if (!scheduleResult) {
       return <p>No schedule generated yet. Click "Run Optimized Schedule" to generate one.</p>;
     }
-    const { max_value, min_rooms, schedule } = scheduleResult;
+    const { max_value, min_rooms, assignments } = scheduleResult;
+    
     return (
       <div style={{ border: "2px solid green", padding: "20px", marginTop: "20px" }}>
         <h3>✅ Schedule Generated!</h3>
         <p><strong>Max Value Achieved:</strong> {max_value}</p>
         <p><strong>Minimum Rooms Used:</strong> {min_rooms}</p>
-  
+        
         <h4>Assignments:</h4>
-        {schedule.assignments && schedule.assignments.length > 0 ? (
+        {assignments && assignments.length > 0 ? (
           <table border="1" cellPadding="10">
             <thead>
               <tr>
@@ -19,7 +20,7 @@ export default function ScheduleView({ scheduleResult }) {
               </tr>
             </thead>
             <tbody>
-              {schedule.assignments.map((a) => (
+              {assignments.map((a) => (
                 <tr key={a.id}>
                   <td>{a.class_session}</td>
                   <td>{a.room}</td>
